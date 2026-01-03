a38a0725463cda7034cde4d503d9ea0af17c499e
Durss <francois.dursus@gmail.com>
2026-01-03 17:51:13 +0100

    chore: disable circular deps check from node_modules
    Reducing prettier max line width

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,49 +1,53 @@
-import commonjs from "@rollup/plugin-commonjs";
-import nodeResolve from "@rollup/plugin-node-resolve";
-import terser from "@rollup/plugin-terser";
-import typescript from "@rollup/plugin-typescript";
-import path from "node:path";
-import url from "node:url";
+import commonjs from '@rollup/plugin-commonjs';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
+import typescript from '@rollup/plugin-typescript';
+import path from 'node:path';
+import url from 'node:url';
+import circular from 'rollup-plugin-circular-dependencies';
 
 const isWatching = !!process.env.ROLLUP_WATCH;
-const sdPlugin = "fr.twitchat.sdPlugin";
+const sdPlugin = 'fr.twitchat.sdPlugin';
 
 /**
  * @type {import('rollup').RollupOptions}
  */
 const config = {
-	input: "src/plugin.ts",
+	input: 'src/plugin.ts',
 	output: {
 		file: `${sdPlugin}/bin/plugin.js`,
 		sourcemap: isWatching,
 		sourcemapPathTransform: (relativeSourcePath, sourcemapPath) => {
 			return url.pathToFileURL(path.resolve(path.dirname(sourcemapPath), relativeSourcePath)).href;
-		}
+		},
 	},
 	plugins: [
 		{
-			name: "watch-externals",
+			name: 'watch-externals',
 			buildStart: function () {
 				this.addWatchFile(`${sdPlugin}/manifest.json`);
 			},
 		},
 		typescript({
-			mapRoot: isWatching ? "./" : undefined
+			mapRoot: isWatching ? './' : undefined,
 		}),
 		nodeResolve({
 			browser: false,
-			exportConditions: ["node"],
-			preferBuiltins: true
+			exportConditions: ['node'],
+			preferBuiltins: true,
 		}),
 		commonjs(),
 		!isWatching && terser(),
 		{
-			name: "emit-module-package-file",
+			name: 'emit-module-package-file',
 			generateBundle() {
-				this.emitFile({ fileName: "package.json", source: `{ "type": "module" }`, type: "asset" });
-			}
-		}
-	]
+				this.emitFile({ fileName: 'package.json', source: `{ "type": "module" }`, type: 'asset' });
+			},
+		},
+		circular({
+			exclude: /node_modules/,
+		}),
+	],
 };
 
 export default config;
