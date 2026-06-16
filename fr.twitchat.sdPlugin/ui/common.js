6b8f5b08a370d7d5aa3c387dd082cb87803b6c4e
Durss <francois.dursus@gmail.com>
2026-06-16 19:13:23 +0200

    chore: internalizing elgato scripts

--- fr.twitchat.sdPlugin/ui/common.js
+++ fr.twitchat.sdPlugin/ui/common.js
@@ -1,4 +1,4 @@
-import { html } from 'https://cdn.jsdelivr.net/gh/lit/dist@3/core/lit-core.min.js';
+import { html } from './lit-core.min.js';
 
 document.body.classList.add('hide');
 const loader = document.createElement('sdpi-item');
